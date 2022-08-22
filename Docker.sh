a1eb6180d456978edf5425d9ef902d509733a2a3
Aiden <53722553+Aiden181@users.noreply.github.com>
2022-08-22 11:35:29 +0700

    Create Docker.sh

---
+++ Docker.sh
@@ -0,0 +1,18 @@
+cd ~
+
+#Import Repository and Key
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+#Install Java
+sudo amazon-linux-extras install java-openjdk11
+
+#Install Jenkin
+yum install jenkins
+
+#Download and Extract Maven
+cd /opt
+wget https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz
+tar -xvzf apache-maven-3.8.6-bin.tar.gz
+mv apache-maven-3.8.6 maven
+cd ~
